cab49b1de0e04ec9fa8b69a7f4e41af74de98cce
petrov-tech <110338544+petrov-tech@users.noreply.github.com>
2022-10-31 09:55:15 +0000

    Deploying to gh-pages from @ petrov-tech/team-test-foodsta@3d910e70e880c04d7b2d76a822e52c29cea7fa81 🚀

---
+++ index.a8c77b11.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a8c77b11.js.map
